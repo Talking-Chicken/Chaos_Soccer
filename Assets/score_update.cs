4d3ede95890f574e0262dbb126f8e9967a582ca4
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2021-11-28 17:55:05 -0500

    score_system_2 #update

--- Assets/score_update.cs
+++ Assets/score_update.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class score_update : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static int score_left=0;
+    public static int score_right=0;
+
+    public TextMeshProUGUI score_1;
+    public TextMeshProUGUI score_2;
     void Start()
     {
         
@@ -13,6 +20,7 @@ public class score_update : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        score_1.text = score_left.ToString();
+        score_2.text = score_right.ToString();
     }
 }
