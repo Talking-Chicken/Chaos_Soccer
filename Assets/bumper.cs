d7a39ad75d8d6d7cefa172c9ed647f0d2ea87d9b
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2021-12-08 11:54:43 -0500

    animation_bumper

--- Assets/bumper.cs
+++ Assets/bumper.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class bumper : MonoBehaviour
 {
     // Start is called before the first frame update
+    Animator m_Animator;
+
     void Start()
     {
-        
+        m_Animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name== "Soccer ball")
+        {
+            m_Animator.SetTrigger("hit");
+        }
     }
 }
