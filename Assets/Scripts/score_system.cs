c7d5ee6aa2a8a49bf67c28c405482051f2166d26
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2021-12-10 23:51:57 -0500

    hotfix

--- Assets/Scripts/score_system.cs
+++ Assets/Scripts/score_system.cs
@@ -9,6 +9,8 @@ public class score_system : MonoBehaviour
     public GameObject soccer;
     public bool left;
     public bool right;
+    public GameObject[] blue_team_firework;
+    public GameObject[] red_team_firework;
     
     void Start()
     {
@@ -24,27 +26,47 @@ public class score_system : MonoBehaviour
     {
         if (collision.gameObject.name== "Soccer ball")
         {
-            Debug.Log("test");
+            
             StartCoroutine(ExampleCoroutine());
             if (left == true)
             {
                 score_update.score_left = score_update.score_left + 1;
+                Camera.main.transform.position = new Vector3(71.8f, -10f, -10);
+                Camera.main.orthographicSize = 20;
+
+                for (int i = 0; i < blue_team_firework.Length; i++)
+                {
+                    blue_team_firework[i].GetComponent<ParticleSystem>().Play();
+                  
+                }
             }
             if (right == true) { 
                 score_update.score_right = score_update.score_right + 1;
+                Camera.main.transform.position = new Vector3(-81.1f, -10f, -10);
+                Camera.main.orthographicSize = 20;
+                for (int i = 0; i < red_team_firework.Length; i++)
+                {
+                    red_team_firework[i].GetComponent<ParticleSystem>().Play();
+                }
             }
         }
     }
    
-
+    void camera_reset()
+    {
+        Camera.main.transform.position = new Vector3(0,18,-10);
+        Camera.main.orthographicSize = 80;
+    }
 
 IEnumerator ExampleCoroutine()
 {
 
-    //yield on a new YieldInstruction that waits for 5 seconds.
-    yield return new WaitForSeconds(0.5f);
+    //yield on a new YieldInstruction that waits for 2 seconds.
+    yield return new WaitForSeconds(4f);
         soccer.transform.position = new Vector3(-4.5f, -14.21f, 0);
-        soccer.GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0);
+        soccer.GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0); 
+        soccer.GetComponent<Rigidbody2D>().freezeRotation = true;
+        camera_reset();
         foreach (GameObject goat in TeamManager.team1)
         {
             goat.GetComponentInChildren<Athelete>().gameObject.transform.position=goat.GetComponentInChildren<Athelete>().spawnPos;
